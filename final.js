18e987c205427dd64aea29b39d51f87e37d8410e
Tebbo <leetebbs@ymail.com>
2022-03-09 22:20:10 +0000

    Add find id from username

--- final.js
+++ final.js
@@ -1,4 +1,3 @@
-
 const Web3 = require('web3');
 const badgeAbi = require('./abibadge.json');
 const fetch = require('node-fetch');;
@@ -24,73 +23,58 @@ async function listen(){
             fromUser = event.returnValues._from;
             useId = event.returnValues.userId;
             test= event.returnValues
-            console.log(useId);
-            console.log(fromUser);
-            console.log('Test',test);
-            //send message to discord to trigger role add
             const whurl = process.env.WHURL
             const msg = {
                 "content": "Badge_Holder_Added "
             }
 
-
             fetch(whurl + "?wait=true", 
             {"method":"POST", 
             "headers": {"content-type": "application/json"},
             "body": JSON.stringify(msg)})
-            .then(a=>a.json()).then(console.log)
+            .then(a=>a.json())//.then(console.log)
            
     })
-     .on('error', console.error);
-     setTimeout(listen,40000);
-     console.log('pinged by timer ⏰');
-
+        .on('error', console.error);
+        setTimeout(listen,40000);
+        console.log('pinged by timer ⏰');
 
 }
 
-//set up bot
+//********set up bot***********
 
 client.login(process.env.TOKEN)
 
 async function botSetup(){
     
-
     client.on('ready', () => {
         console.log('The bot is ready 🤖');
 
     })
     
-    //Adds Role when contract event emitted
+    //Adds Role and pemissions to channels when contract event emitted
     client.on('messageCreate', message =>{
         if(message.content === 'Badge_Holder_Added') {
             if(!message.author.bot) return;
-            //const idNumber= client.users.cache.find(u => u.tag === useId).id
-            console.log(useId);
-            const guild = client.guilds.cache.get(process.env.CHANNEL_ID) //channel id
-            //let id = "830528936357396490" //mine
-            let id = useId  // user id tester213
-            let roles = process.env.ROLE_ID //NFT role id
-            guild.members.fetch(id).then(member=>member.roles.add(roles)) 
-            console.log(id) 
+            const guild = client.guilds.cache.get('948929783817990194');
+            guild.members.fetch().then(members =>
+            {
+                members.forEach(member =>
+                {
+                    if (useId === member.user.username){
+                        let newID = member.user.id
+                        console.log(member.user.username)
+                        console.log(newID);
+                        let id = newID 
+                    let roles = process.env.ROLE_ID //NFT role id
+                    guild.members.fetch(id).then(member=>member.roles.add(roles))
+                        return
+                    }
+                });
+            });
+           
         }
-
     });
-
-
-        client.on('messageCreate', (message) => {
-        if(message.content === 'P') {
-            message.reply({
-                content: 'pong',    
-            })
-        }else if(message.content=== 'n') {
-            
-            message.channel.send(`Total memebers online : ${message.guild.maximumMembers}`);
-            message.channel.send(`username : ${message.author.username}`);
-            message.channel.send(`ID : ${message.author.id}`)
-            message.channel.send(`ID : ${message.id}`)
-        }
-
-    })
 
     
       }
