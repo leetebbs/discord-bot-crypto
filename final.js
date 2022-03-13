985b76f9812b7e7bf1dec2c83e55a8ce3773f082
Tebbo <leetebbs@ymail.com>
2022-03-13 13:32:59 +0000

    slim down final, remove packages that are not needed

--- final.js
+++ final.js
@@ -1,81 +1,45 @@
 const Web3 = require('web3');
 const badgeAbi = require('./abibadge.json');
-const fetch = require('node-fetch');;
 require('dotenv').config();
-//const web3 = new Web3('https://rpc-testnet.kcc.network')
-const web3 = new Web3('wss://rpc-ws-testnet.kcc.network') //websocket
-const address = process.env.CONTRACT_ADDRESS;// testbadge contract
+const web3 = new Web3('wss://rpc-ws-testnet.kcc.network');
+const address = process.env.CONTRACT_ADDRESS;
 const contract = new web3.eth.Contract(badgeAbi, address);
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
-require('dotenv').config();
+client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+    console.log('The bot is ready 🤖');
 listen();
-botSetup();
-
-//Listen on websocket to contract badgeAttached event
 async function listen(){
-    contract.events.badgeAttached({  //Name of the Event to listen to emitted by the contract
-        filter: {myIndexedParam: [20,23], myOtherIndexedParam: ''}, // Using an array means OR: e.g. 20 or 23
+    contract.events.badgeAttached({  
+        filter: {myIndexedParam: [20,23], myOtherIndexedParam: ''}, 
         fromBlock: 'latest'
-        
     }, function(error, event){ 
+        
+        fromUser = event.returnValues._from;
+        useId = event.returnValues.userId;
+        test= event.returnValues;
 
-            fromUser = event.returnValues._from;
-            useId = event.returnValues.userId;
-            test= event.returnValues
-            const whurl = process.env.WHURL
-            const msg = {
-                "content": "Badge_Holder_Added "
-            }
-
-            fetch(whurl + "?wait=true", 
-            {"method":"POST", 
-            "headers": {"content-type": "application/json"},
-            "body": JSON.stringify(msg)})
-            .then(a=>a.json())//.then(console.log)
-           
+        const guild = client.guilds.cache.get(process.env.SERVER_ID);
+        guild.members.fetch().then(members =>
+        {
+            members.forEach(member =>
+            {
+                let newID = member.user.id
+                let userName = member.user.username
+                let id = newID 
+                if(useId !== userName){
+                    return
+                }else{
+                    let roles = process.env.ROLE_ID
+                    guild.members.fetch(id).then(member=>member.roles.add(roles))
+                }
+            });
+        });       
     })
         .on('error', console.error);
         setTimeout(listen,30000);
         console.log('pinged by timer ⏰');
-
 }
-
-//********set up bot***********
-
-client.login(process.env.TOKEN)
-
-async function botSetup(){
-    
-    client.on('ready', () => {
-        console.log('The bot is ready 🤖');
-
-    })
-    
-    //Adds Role and pemissions to channels when contract event emitted
-    client.on('messageCreate', message =>{
-        if(message.content === 'Badge_Holder_Added') {
-            if(!message.author.bot) return;
-            const guild = client.guilds.cache.get(process.env.SERVER_ID);
-            guild.members.fetch().then(members =>
-            {
-                members.forEach(member =>
-                {
-                    if (useId === member.user.username){
-                        let newID = member.user.id
-                        console.log(member.user.username)
-                        console.log(newID);
-                        let id = newID 
-                    let roles = process.env.ROLE_ID //NFT role id
-                    guild.members.fetch(id).then(member=>member.roles.add(roles))
-                        return
-                    }
-                });
-            });
-           
-        }
-    });
-
-    
-}
-
+})
